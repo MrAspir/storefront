3eceb61c9b8ccf043109d271730a4ebc698f5e95
Roman Krasinskyi <krasinskiy.r.i@gmail.com>
2019-02-19 00:22:39 +0200

    Added Header component

---
+++ src/component/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Header = () => (
+    <div className="Header">
+        <div className="Header__container">
+            <div className="Header__row">
+                <div className="Header__logo">
+
+                </div>
+
+                <div className="Header__menu">
+
+                </div>
+
+                <div className="Header__cart">
+
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Header;
