24fb9aca84b9df3a7132371913cce7f33a89983c
Roman Krasinskyi <krasinskiy.r.i@gmail.com>
2019-02-20 00:34:39 +0200

    Added Copyright component

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Copyright from './component/Copyright/Copyright';
+
 const Footer = () => (
     <footer className="Footer">
         <div className="Footer__container">
             <div className="Footer__row">
                 <div className="Footer__copyright">
+                    <Copyright />
                 </div>
 
                 <div className="Footer__developed">
---
+++ src/component/Footer/component/Copyright/Copyright.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Copyright = () => (
+    <div className="copyright">&copy; Copyright {new Date().getFullYear()}</div>
+);
+
+export default Copyright;
